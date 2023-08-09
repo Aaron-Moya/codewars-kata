7c204de7f75f11281bfcfd6d4bbe8c5883b39204
AARON\drake <aaronmoyaarques@gmail.com>
2023-08-09 22:48:40 +0200

    statement

--- dart/count_sheep.dart
+++ dart/count_sheep.dart
@@ -1,3 +1,5 @@
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
 String countSheep(int numb) {
   String result = "";
   for (int i = 0; i < numb; i++) result += ((i + 1).toString() + " sheep...");
